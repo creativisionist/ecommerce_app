9608361d2f9ace479be73d64683d9c5b71f6fccc
creativisionist <creativisionist@gmail.com>
2015-06-14 14:26:10 -0500

    make category available on product via join table

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ActiveRecord::Base
   has_many :product_images
   has_many :orders
   has_many :categorized_products
+  has_many :categories, through: :categorized_products
 
   SALES_TAX = 0.09
 
