e9cfaeeefb2f88dd50092fc38f93c678000e7e92
creativisionist <creativisionist@gmail.com>
2015-06-16 19:27:39 -0500

    create create and index methods

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -7,6 +7,6 @@ class CartedProductsController < ApplicationController
 
   def create
     @order = CartedProduct.create(quantity: params[:quantity], product_id: params[:product_id], user_id: current_user.id, status: "carted")
-    redirect_to "/carted_product/#{carted_product.id}"
+    redirect_to "/carted_products"
   end
 end
