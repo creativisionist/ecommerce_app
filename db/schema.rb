c4ba0a590c5c47ac27adaba46c8f29cfc0c81f48
creativisionist <creativisionist@gmail.com>
2015-06-17 18:17:15 -0500

    rm product_id column from orders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150617010934) do
+ActiveRecord::Schema.define(version: 20150617231625) do
 
   create_table "carted_products", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 20150617010934) do
     t.integer  "user_id",    limit: 4
     t.datetime "created_at",                                    null: false
     t.datetime "updated_at",                                    null: false
-    t.integer  "product_id", limit: 4
     t.decimal  "subtotal",             precision: 10, scale: 2
     t.decimal  "tax",                  precision: 5,  scale: 2
     t.decimal  "total",                precision: 10, scale: 2
