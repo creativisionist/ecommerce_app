41825f10728a53a73346654d5a5b09a85b5865d8
creativisionist <creativisionist@gmail.com>
2015-06-16 23:00:08 -0500

    update to calculate subtotal, tax, total of carted_products for order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,26 @@
 class OrdersController < ApplicationController
 
   def show
-    @order = Order.find_by(id: params[:id])
+
   end
 
   def create
     order = Order.create
     items_to_be_purchased = current_user.carted_products.where(status: "carted")
-    items_to_be_purchased each do |item|
+    items_to_be_purchased.each do |item|
       item.update(order_id: order.id, status: "purchased")
     end
-    order.carted_products each do |item|
-      
+
+    subtotal = 0
+    order.carted_products.each do |item|
+      price = Product.all.find_by(id: item.product_id).price
+      quantity = item.quantity
+      subtotal += (price * quantity)
     end
+    tax = subtotal * 0.09
+    total = subtotal + tax
+    order.update(subtotal: subtotal, tax: tax, total: total)
+
+    redirect_to "/orders/#{order.id}"
   end
 end
