6dd7d44047cf7cc2b945f3a27e0d657e3888f369
Creativisionist <creativisionist@gmail.com>
2015-06-18 19:48:32 -0500

    have validations show

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,12 +1,19 @@
 class CartedProductsController < ApplicationController
 
+  before_action :authenticate_user
+
   def index
     @carted_product = CartedProduct.all
     @products = Product.all
   end
 
   def create
-    @order = CartedProduct.create(quantity: params[:quantity], product_id: params[:product_id], user_id: current_user.id, status: "carted")
-    redirect_to "/carted_products"
+    @order = CartedProduct.new(quantity: params[:quantity], product_id: params[:product_id], user_id: current_user.id, status: "carted")
+    @product = Product.find_by(id: params[:product_id])
+    if @carted_product.save
+      redirect_to "/carted_products"
+    else
+      render "/products/show"
+    end
   end
 end
