d9738a771f05f137873c05bf297e7adcd828de0a
Creativisionist <creativisionist@gmail.com>
2015-06-18 19:09:36 -0500

    add validations

--- app/models/carted_product.rb
+++ app/models/carted_product.rb
@@ -2,4 +2,8 @@ class CartedProduct < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
   belongs_to :user
+
+  validates :quantity, presence: true
+  validates :quantity, numericality: { only_integer: true }
+  validates :quantity, numericality: { greater_than: 0}
 end
