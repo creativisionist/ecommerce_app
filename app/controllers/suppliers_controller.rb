5808a557b3dec75c96f5e1943015aa558c8aaa43
Creativisionist <creativisionist@gmail.com>
2015-06-21 11:35:12 -0500

    update and create suppliers form and routes

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,2 +1,34 @@
 class SuppliersController < ApplicationController
+
+  def index
+    @suppliers = Supplier.all
+  end
+
+  def new
+    @supplier = Supplier.new
+  end
+
+  def show
+    @supplier = Supplier.find_by(id: params[:id])
+  end
+
+  def create
+    supplier = Supplier.create(params[:supplier])
+    redirect_to "/suppliers/#{supplier.id}"
+  end
+
+  def edit
+    @supplier = Supplier.find_by(id: params[:id])
+  end
+
+  def update
+    supplier = Supplier.find_by(id: params[:id])
+    supplier.update(params[:supplier])
+    redirect_to '/suppliers'
+  end
+
+  def destroy
+    Supplier.find_by(id: params[:id]).destroy
+    redirect_to '/suppliers'
+  end
 end
